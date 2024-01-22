7e5f0a22ae1cc9d6bb272ddb92435949c8682a29
Helia Seyedmazhari <71783659+HeliaSeyedmazhari@users.noreply.github.com>
2024-01-22 12:25:52 -0500

    version 1.0.1

--- ps1.py
+++ ps1.py
@@ -10,8 +10,33 @@ def spy263(commands):
   '''
   Pre: commands is a list of commands
   Post: return list of find_spy results
+  Commands
+  'insert x'
+	    -Inserts the number x into the data structure in spy263
+	    - Must take O(log n) time for worst case
+  'find_spy'
+	    On the data structure of size n, returns ceil(0.263*n)th smallest element
+	    Must take O(1) time for worst case 
   '''
   pass # TODO: implement this function
+  data_structure = []
+  for command in commands:
+    if command.split(" ")[0] == "insert":
+      insert(data_structure, command.split(" ")[1])
+    elif command.split(" ")[0] == "find_spy":
+      find_spy(data_strucutre, len(data_structure))
+
+def insert(data_structure,x):
+  """
+  inserts the number <x> into the <data strcture>
+  """
+  pass
+
+def find_spy(data_structure, n):
+  """
+  On the <data structure> of size <n>, returns ceil(0.263*n)th smallest element
+  """
+  omega = 0.263
 
 # You may add additional test case below. HOWEVER, your code must
 # compile and be runnable in order to earn any credit for correctness.
